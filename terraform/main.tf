d7999a16c9eebf19f7ef599731a113891ba56ff9
Guilherme Varela <guilhermevareladossantos@gmail.com>
2024-08-13 23:00:37 -0300

    Reutiliza bucket caso ele exista

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,26 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_s3_bucket" "static_site" {
+# Referencia o bucket existente
+data "aws_s3_bucket" "static_site" {
   bucket = "meu-site-estatico-bucket"
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
 }
 
-
+# Adiciona um objeto ao bucket existente
 resource "aws_s3_bucket_object" "index" {
-  bucket = aws_s3_bucket.static_site.bucket
+  bucket = data.aws_s3_bucket.static_site.bucket
   key    = "index.html"
   source = "site/index.html"
   acl    = "public-read"
 }
 
 resource "aws_s3_bucket_object" "error" {
-  bucket = aws_s3_bucket.static_site.bucket
+  bucket = data.aws_s3_bucket.static_site.bucket
   key    = "error.html"
   source = "site/error.html"
   acl    = "public-read"
 }
 
-output "website_url" {
-  value = aws_s3_bucket.static_site.website_endpoint
-}
-
+# Opcional: Adicione uma política ao bucket existente (caso necessário)
 resource "aws_s3_bucket_policy" "static_site_policy" {
-  bucket = aws_s3_bucket.static_site.id
+  bucket = data.aws_s3_bucket.static_site.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -40,9 +29,8 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
         Effect = "Allow"
         Principal = "*"
         Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.static_site.arn}/*"
+        Resource = "${data.aws_s3_bucket.static_site.arn}/*"
       },
     ]
   })
 }
-
